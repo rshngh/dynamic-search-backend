3755f4855a213c0e4397da5fbdc4e47f4c0772b7
rahul <rsngh@outlook.com>
2025-03-23 14:21:05 +0530

    fix

--- index.js
+++ index.js
@@ -3,28 +3,12 @@ import express, { urlencoded } from "express";
 import cors from "cors";
 import connectDB from "./server.js";
 import cityRouter from "./routes/city.routes.js";
-import path from "path";
 
 const app = express();
 const PORT = process.env.PORT;
 
-const __dirname = path.resolve();
+app.use(cors());
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../search-client/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(
-      path.join(__dirname, "../search-client", "dist", "index.html")
-    );
-  });
-}
 try {
   app.listen(PORT, () => {
     console.log(`Successfully connected to the server at port ${PORT}.`);
